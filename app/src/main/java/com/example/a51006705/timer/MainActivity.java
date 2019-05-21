5cf28470ee1ce38b0a0d397d48bf973438f3af7d
Ratna Srivastava <51006705@INDLAVA.COM>
2019-05-21 17:19:00 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/a51006705/timer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a51006705.timer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
